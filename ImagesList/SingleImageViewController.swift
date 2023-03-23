6fbc648fdecbf20192caa88480c5f27d9b13e4b2
777EXX <panda3er@gmail.com>
2023-03-23 11:04:33 +0300

    Sprint_10 попытка 1

--- ImagesList/SingleImageViewController.swift
+++ ImagesList/SingleImageViewController.swift
@@ -11,27 +11,32 @@ final class SingleImageViewController: UIViewController {
     var image: UIImage! {
         didSet {
             guard isViewLoaded else { return }
-            
+            imageView.image = image
             rescaleAndCenterImageInScrollView(image: image)
         }
     }
-
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet private var imageView: UIImageView!
-    @IBOutlet private weak var scrollView: UIScrollView!
-    @IBAction func didTabBackButton(_ sender: Any) {
-        dismiss(animated: true)
-    }
-    @IBAction func didTabSharingButton(_ sender: Any) {
-        
-        let activityViewController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
-        present(activityViewController, animated: true, completion: nil)
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = image
         scrollView.minimumZoomScale = 0.1
         scrollView.maximumZoomScale = 1.25
+        imageView.image = image
         rescaleAndCenterImageInScrollView(image: image)
+    }
+
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: UIButton) {
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
     }
     
     private func rescaleAndCenterImageInScrollView(image: UIImage) {
@@ -42,8 +47,7 @@ final class SingleImageViewController: UIViewController {
         let imageSize = image.size
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
-        let scale = max(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
-        scrollView.maximumZoomScale = scale > scrollView.maximumZoomScale ?  scale : scrollView.maximumZoomScale // для растягивания на весь экран
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
         let newContentSize = scrollView.contentSize
